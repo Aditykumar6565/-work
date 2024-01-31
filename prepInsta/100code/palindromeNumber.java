6276b56f383737e9325f8ea2fb8fcf6d30b27d85
Aditykumar6565 <86483020+Aditykumar6565@users.noreply.github.com>
2024-01-31 04:40:46 +0000

    c

--- prepInsta/100code/palindromeNumber.java
+++ prepInsta/100code/palindromeNumber.java
@@ -1,17 +1,23 @@
-public class palindromeNumber {
-    public static void main(String[] args) {
-        int num = 12021, reverse = 0, rem, temp;
-        temp = num;
-        while (temp != 0) {
-            rem = temp % 10;
-            reverse = reverse * 10 + rem;
-            temp /= 10;
+import java.util.Scanner;
 
-        }
-        ;
-        if (num == reverse)
-            System.out.println(num + " is Palindrome");
-        else
-            System.out.println(num + " is not Palindrome");
-    }
+public class palindromeNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your number:- ");
+    int num = sc.nextInt();
+    int reverse = 0,rem,temp;
+    temp = num;
+    while (temp!=0) {
+        rem = temp%10;
+        reverse = reverse *10 +rem;
+        temp /= 10;
+
+        
+    }if (num==reverse) {
+        System.out.println("The given number  "+num+"  is palindrome number; ");
+
+        
+    }else
+    System.out.println("The given number "+num+"  is not palindrome number; ");
+  }
 }
